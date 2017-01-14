239710aff029f7f4b2b2df1dff4304daede619de
Dietmar Maurer <dietmar@proxmox.com>
2017-01-14 15:42:36 +0100

    Bootstrap.pm: remove support for IE8 and older

--- PVE/APIServer/Formatter/Bootstrap.pm
+++ PVE/APIServer/Formatter/Bootstrap.pm
@@ -6,6 +6,10 @@ use URI::Escape;
 use HTML::Entities;
 use JSON;
 
+# FIXME: page title
+# FIXME: cookie_name
+# FIXME: console code??
+
 # Helpers to generate simple html pages using Bootstrap markup.
 
 my $jssrc = <<_EOJS;
@@ -107,13 +111,6 @@ body {
 	padding-top: 70px;
 }
     </style>
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
