proxmox/pve-http-server

commit f7379848262299629c9c1d0db33295fc94765347

fix #4816: do not disconnect twice if client sends no data
client_do_disconnect expects to be called exactly once per connection, since it
takes care of closing and unsetting the handle corresponding to the connection.
to find bugs in our connection handling, it will log "detected empty handle" if
it is called for a request/connection that no longer has a handle.

the edge case of opening a connection without sending any data leads to the
error callback being called twice:

Dec 04 09:37:02 xxx pveproxy[175235]: err (): Connection timed out

this is the (5 second) timeout triggering

Dec 04 09:37:02 xxx pveproxy[175235]: err (1): Broken pipe

this is AnyEvent trying to drain the buffer while the connection is already
closed

as soon as a single byte of traffic is sent, only the timeout will trigger.

there is no guarantee that the on_error callback is only called once (in fact,
it's possible to return from it for non-fatal errors and continue processing
the connection).

if there are further reports of empty handles with this in place, other
on_error callbacks might need similar logic - but it should only be added if
the triggering conditions are clear and deemed safe. the additional logging is
only cosmetic after all, but might point out an actual issue in our connection
handling code.

Signed-off-by: Fabian Grünbichler <f.gruenbichler@proxmox.com>

src/PVE/APIServer/AnyEvent.pm
@@ -1903,7 +1903,13 @@ sub accept_connections {
 		    my ($hdl, $fatal, $message) = @_;
 		    eval {
 			$self->log_aborted_request($reqstate, $message);
-			$self->client_do_disconnect($reqstate);
+			# this error callback can be called twice for the same
+			# connection/handle if the timeout is reached before
+			# any data has been received, avoid misleading errors
+			if (!$reqstate->{disconnected}) {
+			    $self->client_do_disconnect($reqstate);
+			    $reqstate->{disconnected} = 1;
+			}
 		    };
 		    if (my $err = $@) { syslog('err', "$err"); }
 		},

commit fa639169214f679c90274e4a6f1a84b864212326

anyevent: always avoid re-entering client_do_disconnect() in on_error callback
Commit f737984 ("fix #4816: do not disconnect twice if client sends no
data") introduced a 'disconnected' flag in the request state to avoid
duplicate calls to client_do_disconnect() for a given client. This
works, except in the case where client_do_disconnect() enters the
on_error callback itself. To fix this, set the 'disconnected' flag
before calling client_do_disconnect().

This was exposed by commit 07e56cc ("fix unexpected EOF for client
when closing TLS session") which introduced a call to stoptls() in
client_do_disconnect(). The documentation [0] mentions for stoptls():

> This method may invoke callbacks (and therefore the handle might be
> destroyed after it returns).

Indeed, the on_error callback might get invoked and lead to a
"detected empty handle" error message as reported in the community
forum [1].

[0]: https://metacpan.org/pod/AnyEvent::Handle#$handle-%3Estoptls
[1]: https://forum.proxmox.com/threads/164744/

Fixes: 07e56cc ("fix unexpected EOF for client when closing TLS session")
Signed-off-by: Fiona Ebner <f.ebner@proxmox.com>
Link: https://lore.proxmox.com/20250408142014.86344-3-f.ebner@proxmox.com

src/PVE/APIServer/AnyEvent.pm
@@ -1915,8 +1915,8 @@ sub accept_connections {
 			# connection/handle if the timeout is reached before
 			# any data has been received, avoid misleading errors
 			if (!$reqstate->{disconnected}) {
-			    $self->client_do_disconnect($reqstate);
 			    $reqstate->{disconnected} = 1;
+			    $self->client_do_disconnect($reqstate);
 			}
 		    };
 		    if (my $err = $@) { syslog('err', "$err"); }