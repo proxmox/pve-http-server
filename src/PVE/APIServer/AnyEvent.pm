proxmox/pve-http-server

commit 07e56cc9dd8c1d3354aaf317adaac109437e80f7

fix unexpected EOF for client when closing TLS session
When pve-http-server initiates the closure of a TLS session, it does not
send a TLS close notify, resulting in an unexpected EOF error on systems
with recent crypto policies. This can break functionality with other
applications, such as Foreman[0].

This behavior can be observed in the following cases:

 * client uses HTTP/1.0 (no keepalive; server closes connection)
 * client sends no data for 5 sec (timeout; server closes connection)
 * server responds with 400 (no keepalive; server closes connection)

This patch sends the TLS close notify prior to socket teardown,
resulting in clean closure of TLS connections and no client error.

It also moves shutdown() to after the clearing of handlers. The reason
for this is stoptls() must come before shutdown(), but it also triggers
on_drain(), which calls client_do_disconnect() again. The extra call to
client_do_disconnect() is avoided inside accept_connections() by commit
f737984, but perhaps clearing the handlers prior to shutdown() will
avoid it in all cases.

[0]: https://github.com/theforeman/foreman_fog_proxmox/issues/325

Signed-off-by: Rob Rozestraten <admin@truthsolo.net>
Link: https://lore.proxmox.com/mailman.798.1741211145.293.pve-devel@lists.proxmox.com
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

src/PVE/APIServer/AnyEvent.pm
@@ -141,11 +141,13 @@ sub client_do_disconnect {
     my $shutdown_hdl = sub {
 	my $hdl = shift;
 
-	shutdown($hdl->{fh}, 1);
 	# clear all handlers
 	$hdl->on_drain(undef);
 	$hdl->on_read(undef);
 	$hdl->on_eof(undef);
+
+	$hdl->stoptls();
+	shutdown($hdl->{fh}, 1);
     };
 
     if (my $proxyhdl = delete $reqstate->{proxyhdl}) {

commit 0ec627cff14a51695e6f81f86b34b398b07cf329

anyevent: disconnect: check that handle is still defined before calling shutdown()
Commit 07e56cc ("fix unexpected EOF for client when closing TLS
session") added a call to stoptls() before the call to shutdown() for
the handle's file descriptor. However, the documentation for
AnyEvent[0] mentions for stoptls():

> This method may invoke callbacks (and therefore the handle might be
> destroyed after it returns).

Therefore, it is necessary to check that the handle is still defined
before calling shutdown(). Otherwise, this can result in a warning:

> Can't use an undefined value as a symbol reference at
> /usr/share/perl5/PVE/APIServer/AnyEvent.pm line 150.

as reported in the community forum [1].

The debug print message for closing the file handle is split up,
because part of it relies on the file handle to be defined.

[0]: https://metacpan.org/pod/AnyEvent::Handle#$handle-%3Estoptls
[1]: https://forum.proxmox.com/threads/164744/

Fixes: 07e56cc ("fix unexpected EOF for client when closing TLS session")
Signed-off-by: Fiona Ebner <f.ebner@proxmox.com>
Link: https://lore.proxmox.com/20250408142014.86344-2-f.ebner@proxmox.com

src/PVE/APIServer/AnyEvent.pm
@@ -146,8 +146,11 @@ sub client_do_disconnect {
 	$hdl->on_read(undef);
 	$hdl->on_eof(undef);
 
-	$hdl->stoptls();
-	shutdown($hdl->{fh}, 1);
+	$self->dprint("CLOSE FH" .  $hdl->{fh}->fileno());
+
+	$hdl->stoptls(); # can invoke callbacks and destroy the handle
+
+	shutdown($hdl->{fh}, 1) if defined($hdl) && defined($hdl->{fh});
     };
 
     if (my $proxyhdl = delete $reqstate->{proxyhdl}) {
@@ -170,7 +173,7 @@ sub client_do_disconnect {
 
     $self->{conn_count}--;
 
-    $self->dprint("CLOSE FH" .  $hdl->{fh}->fileno() . " CONN$self->{conn_count}");
+    $self->dprint("DISCONNECT CONN$self->{conn_count}");
 }
 
 sub finish_response {